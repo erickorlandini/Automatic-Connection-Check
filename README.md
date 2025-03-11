ee80adfb7967ac5c3bb4ce541c4ecb8933ed89be
erick.aleixo <erick.aleixo@quickcomex.com.br>
2025-03-11 14:23:03 -0300

    Automação de verificação de conexão

---
+++ README.md
@@ -0,0 +1,12 @@
+# Automatic Connection Check
+
+Esta automação foi desenvolvida afim de facilitar a verificação de estabilidade de algum site utilizando request direta mais uma verificação de login. Neste projeto foi abordado o Python com Selenium.
+
+## ✔️ Técnicas e tecnologias utilizadas
+<img src="https://img.shields.io/badge/-Python-%23333?style=for-the-badge&logo=python&logoColor=white" target="_blank"></a>
+
+<img src="https://img.shields.io/badge/-Selenium-%23333?style=for-the-badge&logo=selenium&logoColor=white" target="_blank"></a>
+
+## 🛠️ Abrir e rodar o projeto
+
+Para abrir e rodar o projeto, execute `python3 src/checkConnection.py` para inicar o projeto.
---
+++ src/checkConnection.py
@@ -0,0 +1,55 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import requests
+
+USER = "erick.aleixo@quickcomex.com.br"
+PASSWORD = "quick@123"
+
+def verificar_site_selenium(url):
+    driver = webdriver.Chrome()  # Ou Edge, Firefox...
+
+    try:
+
+        driver.get(url)
+        time.sleep(4)
+    
+        if "Entrar em Quickcomex" in driver.title:
+            print(f"✅ {url} Login efetuado com sucesso, site esta OK!")
+        else:
+            print(f"⚠️ {url} pode estar com problemas.")
+            driver.quit()
+            return
+
+        campo_user = driver.find_element(By.ID, "username")
+        campo_password = driver.find_element(By.ID, "password")
+        botao_login = driver.find_element(By.ID, "kc-login")
+
+        campo_user.send_keys(USER)
+        campo_password.send_keys(PASSWORD)
+
+        botao_login.click()
+        time.sleep(5)
+
+        if "" in driver.current_url:
+            print("✅ Login realizado com sucesso!")
+        else:
+            print("❌ Falha no login. Verifique suas credenciais.")
+    
+    except Exception as e:
+        print(f"⚠️ Erro: {e}")
+    
+    try:
+        resposta = requests.get(url, timeout=5)
+        if resposta.status_code == 200:
+            print(f"✅ {url} está ONLINE!")
+        else:
+            print(f"⚠️ {url} está com problema! Código: {resposta.status_code}")
+    except requests.exceptions.RequestException:
+        print(f"❌ {url} está OFFLINE!")
+
+    finally:
+        driver.quit()
+
+# Testando
+verificar_site_selenium("https://dbschenker.quickcomex.com.br/")
